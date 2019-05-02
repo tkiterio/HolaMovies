11eaaf629ff875132e25ce4dabff053611d42b2c
shadowcoder89 <50188309+shadowcoder89@users.noreply.github.com>
2019-05-01 22:28:41 -0400

    Add required structure for now.json

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 const {serveHTTP, publishToCentral} = require("stremio-addon-sdk");
 const inEsUsForYou = new (require("./build/InEsUsForYou").InEsUsForYou)();
 
-module.exports = serveHTTP(inEsUsForYou.getInterface(), {port: 56641});
+serveHTTP(inEsUsForYou.getInterface(), {port: 56641})
+    .then(response => {
+        module.exports = response.server;
+    })
+
+
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying.md
