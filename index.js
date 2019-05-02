01f1efaa28da0855177feb38a10321d81a16238b
shadowcoder89 <50188309+shadowcoder89@users.noreply.github.com>
2019-05-01 22:11:34 -0400

    add required structure for now.json

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const inEsUsForYou = new (require("./build/InEsUsForYou").InEsUsForYou)();
 
 serveHTTP(inEsUsForYou.getInterface(), {port: 56641});
 
+module.exports = (req, res) => {
+    res.end(`¡Hola! Movies`)
+}
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying.md
