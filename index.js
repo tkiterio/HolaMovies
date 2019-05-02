3d924784bf714d5b83534415bcb4d0f5885cc4f5
Domingo Sambo <domingosambo@gmail.com>
2019-05-01 22:53:51 -0400

    Env file implemented and port by env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const {serveHTTP, publishToCentral} = require("stremio-addon-sdk");
 const inEsUsForYou = new (require("./build/InEsUsForYou").InEsUsForYou)();
 
-serveHTTP(inEsUsForYou.getInterface(), {port: 56641});
+serveHTTP(inEsUsForYou.getInterface(), {port: process.env.PORT || 56641});
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
