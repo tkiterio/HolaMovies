e4520d9b1776751b1d6eb91084da6567b6d5ee61
Domingo Sambo <domingosambo@gmail.com>
2019-05-18 06:09:11 -0400

    Change movie.js poster url

--- build/Movie.js
+++ build/Movie.js
@@ -14,7 +14,7 @@ class Movie {
             id: this._data.id,
             name: this._data.name,
             type: this._data.type,
-            poster: `https://images.metahub.space/poster/medium/${this._data.id}/img`,
+            poster: `https://images.metahub.space/poster/small/${this._data.id}/img`,
         };
     }
     get magnet() {
